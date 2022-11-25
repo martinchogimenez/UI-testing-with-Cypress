07eac216aa6e0ff386f60a7bd3e826878c2ac775
martinchogimenez <33810604+martinchogimenez@users.noreply.github.com>
2022-11-25 14:47:48 -0300

    Adding come functionalities to my test

---
+++ cypress/e2e/my_first_test.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+
+it('google test', function() {
+
+    cy.visit('https://google.com')
+    cy.get('.gLFyf').type('Automation Step by Step{enter}')
+    cy.wait(4000)
+    cy.contains('Videos').click()
+    
+})
+
+it.only('login test', function() {
+
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+    cy.get('.--visited > .oxd-topbar-body-nav-tab-item').click()
+    cy.get('.oxd-topbar-body-nav-tab-link').click()
+    cy.get('.orangehrm-header-container > .oxd-button').click()
+    cy.get('.oxd-button--secondary').click()
+
+
+})
